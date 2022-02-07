2a2999e07ea2abde336bd9c80f3a0ce39be4492f
callahantiff <callahantiff@gmail.com>
2022-02-07 12:09:22 -0500

    removing extra space

--- enclave_wrangler/config.py
+++ enclave_wrangler/config.py
@@ -2,7 +2,6 @@
 import os
 from dotenv import load_dotenv
 
-
 APP_ROOT = os.path.dirname(os.path.realpath(__file__))
 PROJECT_ROOT = os.path.join(APP_ROOT, '..')
 ENV_DIR = os.path.join(PROJECT_ROOT, 'env')
